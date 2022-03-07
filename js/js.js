8f2c4471fd85f7bbb31060d0521bbf1d6ef2d261
celerno@gmail.com <karma.karma>
2022-03-07 01:04:09 -0700

    enviar mensajes

--- js/js.js
+++ js/js.js
@@ -20,6 +20,23 @@ $('.single-item').slick({
     arrows: false,
     adaptativeHeight: true
 });
+$('#prospectos').submit(function(e) {
+    e.preventDefault();
+    let datos = $(e.target).serializeObject();
+    let url = 'https://script.google.com/macros/s/AKfycbyjJ0xWlF5ChTWPm5EjGO2McmqTQptyG_4WRonYw6o/exec';
+    $.ajax({
+        url: url,
+        method: "POST",
+        dataType: "json",
+        data: datos,
+        success: function() {
+            $('.gracias').fadeIn(1000).fadeOut(300);
+        },
+        error: function(error) {
+            alert(error);
+        }
+    });
+});
 /*
 $('#agendar .section_content>div>div>div:first-of-type').fadeIn();
 */

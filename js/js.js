1223a59068950af58f636d01f90a08851a857a58
celerno@gmail.com <karma.karma>
2022-03-07 22:48:05 -0700

    cambios estilo finales

--- js/js.js
+++ js/js.js
@@ -3,11 +3,15 @@ $("#faq > section.section_content > div > div > div > div:first-child > a").clic
     let n = e.delegateTarget.dataset.n;
     let active = parseInt(e.delegateTarget.dataset.visible);
     //alert(n);
+
     let selector = `#faq > section.section_content > div > div:nth-child(${n}) > div:last-child > div:last-child > span:last-child`;
+    let question = selector.replace('span:last-child', 'span:first-child');
     if (active === 1) {
         $(selector).fadeOut();
         e.delegateTarget.dataset.visible = 0;
+        $(question).css('font-weight', 'normal');
     } else {
+        $(question).css('font-weight', '600');
         $(selector).fadeIn();
         e.delegateTarget.dataset.visible = 1;
     }
